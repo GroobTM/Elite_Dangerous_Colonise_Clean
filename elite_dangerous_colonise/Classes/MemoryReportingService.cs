6aab9c98c767e54e217f33144b1af9bc988a3308
Reuben Wiles Maguire <Reuben.wiles--magu@yorksj.ac.uk>
2025-12-15 22:24:34 +0000

    Added memory reporting service

---
+++ elite_dangerous_colonise/Classes/MemoryReportingService.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Diagnostics;
+using elite_dangerous_colonise.Classes;
+
+namespace elite_dangerous_colonise.Classes
+{
+    public class MemoryReportingService : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            ReportUsage("Service Started");
+            
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    await Task.Delay(TimeSpan.FromHours(1), stoppingToken);
+                    ReportUsage("Hourly Report");
+                }
+                catch (TaskCanceledException) { }
+            }
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            ReportUsage("Service Stopped");
+            return base.StopAsync(cancellationToken);
+        }
+
+        private void ReportUsage(string context)
+        {
+            try
+            {
+                Process proc = Process.GetCurrentProcess();
+                proc.Refresh();
+
+                long physicalMemory = proc.WorkingSet64 / 1024 / 1024;
+                long privateMemory = proc.PrivateMemorySize64 / 1024 / 1024;
+                long heap = GC.GetTotalMemory(false) / 1024 / 1024;
+
+                Logger.LogInformation("Memory Reporting Service", 1,$"{context} | Physical Memory: {physicalMemory}MB | Private Memory: {privateMemory}MB | Managed Heap: {heap}MB");
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("Memory Reporting Service", 2, ex);
+            }
+        }
+    }
+}
